245214a29f39be26cfdb86c36ec0d42e1e189777
Courtney Peters <courtneybpeters@gmail.com>
2014-11-12 12:57:40 -0600

    Changed memory and registers into hex values and bytes, changed keys list into an array of booleans and set timers to start at zeros.

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 #Initialize memory, registers, timers
-memory = ['' for x in range(4096)]
+
+#Must include sections so you don't have to code an offset
+#Bytes are in hex, memory is a list of bytes (8-bits) so it is much more helpful to be in hex.
+memory = [0x00 for x in range(0x1000)]
+
+#Registers
+registers = [0x00 for x in range(0x10)]
+
+
+
 
 #Display - 64 X 32
-#List comprehension of a list comprehension -- an 64 item array of 32 item arrays.
+#List comprehension of a list comprehension -- a 64 item array of 32 item arrays.
 display = [[True for x in range(32)] for x in range(64)]
 
 
 
 
 #Input - Hexidecimal keyboard created using list comprehension
-#QUESTION - Should I convert the integers to strings? Mixed type list seems like a bad idea.
-keys = ['a', 'b', 'c', 'd', 'e', 'f'] + [digit for digit in range(10)]
+#Boolean because we only need to know if key has been pressed or not. Using hex value because the keyboard is oringinally a hex one. 
+keys = [True for x in range(0x10)]
 
 
 
 #Timers - count down until 0
-delay_timer = 60
+delay_timer = 0
 
 #Sound is played only when this is at 0
-sound_timer = 60
+sound_timer = 0
 
 
 
