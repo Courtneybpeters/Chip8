37e492708a8058b07bbaa79e6065b5cbbb34c508
Courtney Peters <courtneybpeters@gmail.com>
2014-12-04 10:06:49 -0600

    Added resize capability.

--- display.py
+++ display.py
@@ -3,31 +3,26 @@ from pygame.locals import *
 
 pygame.init()
 
-screen = pygame.display.set_mode((640, 320))
+size = 10
 
 fps = pygame.time.Clock()
 
 dark = pygame.Color(93, 1, 130)
 light = pygame.Color(229, 204, 255)
 
-x_axis = 0
-y_axis = 0
-
-size = 10
-
 #True -- Dark
 #False -- Light
 display = [[True for x in range(64)] for x in range(32)]
 
 display[0][0] = False
-display[0][1] = False
-display[16][0] = False
 display[0][63] = False
 display[31][0] = False
 display[31][63] = False
 
-
 while True:
+
+    screen = pygame.display.set_mode((64 * size, 32 * size), pygame.RESIZABLE)
+
     for y, row in enumerate(display):
         for x, pixel in enumerate(row):
 
@@ -41,6 +36,12 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+
+        elif event.type == KEYDOWN:
+            if event.key == K_EQUALS:
+                size += 2
+            if event.key == K_MINUS:
+                size -= 2
 
     pygame.display.update()
 
