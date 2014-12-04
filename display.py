e65ec53ae188da190b01800cc3ffc020e8ce76ee
Courtney Peters <courtneybpeters@gmail.com>
2014-12-04 08:01:12 -0600

    Using enumeration as coordinate system.

--- display.py
+++ display.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 
 pygame.init()
 
-screen = pygame.display.set_mode((703, 351))
+screen = pygame.display.set_mode((640, 320))
 
 fps = pygame.time.Clock()
 
@@ -17,24 +17,25 @@ size = 10
 
 #True -- Dark
 #False -- Light
-display = [[[True for x in range(64)], [False for x in range(64)]] for x in range(32)]
+display = [[True for x in range(64)] for x in range(32)]
+
+display[0][0] = False
+display[0][1] = False
+display[16][0] = False
+display[0][63] = False
+display[31][0] = False
+display[31][63] = False
+
 
 while True:
-    for row in display:
-        for col in row:
-            for pixel in col:
-                if x_axis >= 703:
-                    x_axis = 0
-                    y_axis += size + 1
+    for y, row in enumerate(display):
+        for x, pixel in enumerate(row):
 
-                if pixel == True:
-                    screen.fill(dark, (x_axis, y_axis, size, size))
-                    x_axis += size + 1
+            if pixel == True:
+                screen.fill(dark, (x * size, y * size, size, size))
 
-                else:
-                    screen.fill(light, (x_axis, y_axis, size, size))
-                    x_axis += size + 1
-
+            else:
+                screen.fill(light, (x * size, y * size, size, size))
 
     for event in pygame.event.get():
         if event.type == QUIT:
