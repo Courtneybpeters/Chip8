4ba46f4702b6933dafc0173e913341c52f334d77
Courtney Peters <courtneybpeters@gmail.com>
2014-12-10 21:19:18 -0600

    Trying out texts in tkinter.

--- gui.py
+++ gui.py
@@ -1,10 +1,11 @@
 from Tkinter import Tk, Frame, BOTH
 
+
 #Is (Frame) telling it to derive from the class frame that we imported?
 class Window(Frame):
 
     def __init__(self, parent):
-        Frame.__init__(self, parent, background = "black")
+        Frame.__init__(self, parent)
         self.parent = parent
         self.initUI()
         self.centerWindow()
@@ -13,6 +14,10 @@ class Window(Frame):
     def initUI(self):
         self.parent.title("Example GUI")
         self.pack(fill=BOTH, expand = 1)
+        self.grid(column=0, row=0)
+        self.columnconfigure(0, weight = 1)
+        self.rowconfigure(0, weight = 1)
+        intro = Label(self, text = "Chip-8").grid(column=3, row=1, sticky=W, background="white")
 
     def centerWindow(self):
         w = 640
@@ -30,6 +35,7 @@ def main():
 
     root = Tk()
     app = Window(root)
+    ttk.Label(app, text = "Chip 8").grid(column = 3, row = 1, sticky = W, color = "white")
     root.mainloop()
 
 if __name__ == "__main__":
